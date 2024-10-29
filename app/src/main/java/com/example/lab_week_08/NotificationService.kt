03c7a239ab6aa56a67d542e341ff2d09cedde249
Felyks <gamingfelix78@gmail.com>
2024-10-29 16:05:29 +0700

    First Commit

---
+++ app/src/main/java/com/example/lab_week_08/NotificationService.kt
@@ -0,0 +1,104 @@
+package com.example.lab_week_08
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.CountDownTimer
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class NotificationService : Service() {
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+    private lateinit var serviceHandler: Handler
+
+    override fun onBind(intent: Intent): IBinder? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        notificationBuilder = startForegroundService()
+
+        val handlerThread = HandlerThread("SecondThread").apply { start() }
+        serviceHandler = Handler(handlerThread.looper)
+    }
+
+    private fun startForegroundService(): NotificationCompat.Builder {
+        val pendingIntent = getPendingIntent()
+        val channelId = createNotificationChannel()
+        val notificationBuilder = getNotificationBuilder(pendingIntent, channelId)
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        Log.d(
+            "NotificationService",
+            "Foreground service started with notification ID: $NOTIFICATION_ID"
+        )
+        startCountdown()
+        return notificationBuilder
+    }
+
+    private fun getPendingIntent(): PendingIntent {
+        val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) FLAG_IMMUTABLE else 0
+        return PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java), flag)
+    }
+
+    private fun createNotificationChannel(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "001"
+            val channelName = "001 Channel"
+            val channelPriority = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, channelPriority)
+            val service = requireNotNull(
+                ContextCompat.getSystemService(
+                    this,
+                    NotificationManager::class.java
+                )
+            )
+            service.createNotificationChannel(channel)
+            Log.d("NotificationService", "Notification channel created with ID: $channelId")
+            channelId
+        } else {
+            ""
+        }
+    }
+
+    private fun getNotificationBuilder(pendingIntent: PendingIntent, channelId: String) =
+        NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Second worker process is done")
+            .setContentText("Check it out!")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setTicker("Second worker process is done, check it out!")
+            .setOngoing(true)
+            .setContentIntent(pendingIntent)
+
+    private fun startCountdown() {
+        object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsRemaining = millisUntilFinished / 1000
+                notificationBuilder.setContentText("Countdown: $secondsRemaining")
+                val notificationManager =
+                    getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+            }
+
+            override fun onFinish() {
+                mutableID.postValue("001")
+                stopSelf()
+            }
+        }.start()
+    }
+
+    companion object {
+        const val NOTIFICATION_ID = 0xCA7
+        const val EXTRA_ID = "Id"
+        private val mutableID = MutableLiveData<String>()
+        val trackingCompletion: LiveData<String> = mutableID
+    }
+}
